1c4966fcc4c67d062495b8e36819bea83098c336
Aoto Murohara <136954211+muroblue@users.noreply.github.com>
2025-11-04 18:08:55 +0900

    条件分岐を直した

--- syscall.s
+++ syscall.s
@@ -11,21 +11,35 @@
 syscall_handler:
     movem.l %D1-%D7/%A0-%A6, -(%SP)
 
-    cmpi.l #SYSCALL_NUM_GETSTRING, %D0
-    jsr GETSTRING
-    bra END_SYSCALL_HNDR
+	cmpi.l #SYSCALL_NUM_GETSTRING, %D0
+	beq CALL_GETSTRING
 
-    cmpi.l #SYSCALL_NUM_PUTSTRING, %D0
-    jsr PUTSTRING
-    bra END_SYSCALL_HNDR
-
+	cmpi.l #SYSCALL_NUM_PUTSTRING, %D0
+	beq CALL_PUTSTRING
+    
     cmpi.l #SYSCALL_NUM_RESET_TIMER, %D0
-    jsr RESET_TIMER
-    bra END_SYSCALL_HNDR
+    beq CALL_RESET_TIMER
+    
 
-    cmpi.l #SYSCALL_NUM_SET_TIMER, %D0
-    jsr SET_TIMER
-    bra END_SYSCALL_HNDR
+	cmpi.l #SYSCALL_NUM_SET_TIMER, %D0
+	beq CALL_SET_TIMER
+    
+    
 END_SYSCALL_HNDR:
     movem.l (%SP)+, %D1-%D7/%A0-%A6
-    rte
+	rte
+
+CALL_GETSTRING:
+	jsr GETSTRING
+	 bra END_SYSCALL_HNDR
+
+CALL_PUTSTRING:
+	jsr PUTSTRING
+	bra END_SYSCALL_HNDR
+	
+CALL_RESET_TIMER:
+	jsr RESET_TIMER
+	bra END_SYSCALL_HNDR
+CALL_SET_TIMER:
+	jsr SET_TIMER
+	bra END_SYSCALL_HNDR
