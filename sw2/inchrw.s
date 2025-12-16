97c2bd814fa73c7aa4090ca6b476c536ff34e601
Rich1729 <takaaokuro103@gmail.com>
2025-12-16 15:46:36 +0900

    inbyte修正

--- sw2/inchrw.s
+++ sw2/inchrw.s
@@ -1,14 +1,18 @@
 .include "defs.s"
 .global inbyte
 
+|inbyte(ch)	
 .text
 .even
 inbyte:
-    link.w %FP, #-4
-    movem.l %D1-%D3, -(%SP)
+	|chはスタックに積まれている
+	link.w %FP, #-4
+	movem.l %D1-%D3/%A0, -(%SP)
+	move.l %SP, %A0
+	add.l #20, %A0 |20=退避レジスタ数*4 + 4
 inbyte_retry:
     move.l #SYSCALL_NUM_GETSTRING, %D0
-    move.l #0, %D1
+    move.l (%A0), %D1 |ch代入
     move.l %FP, %D2
     subi.l #4, %D2
     move.l #1, %D3
@@ -17,6 +21,6 @@ inbyte_retry:
     bne inbyte_retry
     clr.l %D0
     move.b -4(%FP), %D0
-    movem.l (%SP)+, %D1-%D3
+    movem.l (%SP)+, %D1-%D3/%A0
     unlk %FP
     rts
