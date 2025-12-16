ceb51a73c86ebe56a2886a2bf92b8631c1057177
Rich1729 <takaaokuro103@gmail.com>
2025-12-16 16:00:49 +0900

    outbyte修正

--- sw2/outchr.s
+++ sw2/outchr.s
@@ -1,22 +1,26 @@
 .include "defs.s"
 .global outbyte
 
+|outbyte(ch, c)	
 .text
 .even
 outbyte:
-    # link.w %FP, #0
-    movem.l %D1-%D3, -(%SP)
+	# link.w %FP, #0
+	movem.l %D1-%D3/%A0, -(%SP)
+	move.l %SP, %A0
+	add.l #24, %A0 |24=退避レジスタ数*4 + 4
 outbyte_retry:
-    move.l #SYSCALL_NUM_PUTSTRING, %D0
-    move.l #0, %D1
-    # move.l %FP, %D2
-    # addi.l #11, %D2
-    move.l %SP, %D2
-    addi.l #19, %D2
-    move.l #1, %D3
-    trap #0
-    cmpi.l #0, %D0
-    beq outbyte_retry
-    movem.l (%SP)+, %D1-%D3
-    # unlk %FP
-    rts
+	move.l #SYSCALL_NUM_PUTSTRING, %D0
+	move.l (%A0), %D1 |ch代入
+	#move.l #0, %D1
+	# move.l %FP, %D2
+	# addi.l #11, %D2
+	move.l %SP, %D2
+	addi.l #31, %D2 |31 = 24 + 4(ここがcの末尾) + 3
+	move.l #1, %D3
+	trap #0
+	cmpi.l #0, %D0
+	beq outbyte_retry
+	movem.l (%SP)+, %D1-%D3/%A0
+	# unlk %FP
+	rts
